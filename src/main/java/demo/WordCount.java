6e97b666cb176c6b219e5e14b20fb1c3969999de
Bo Hai <haibo-self@163.com>
2019-07-01 23:48:46 +0800

    stand_alone: fix

--- src/main/java/demo/WordCount.java
+++ src/main/java/demo/WordCount.java
@@ -23,7 +23,7 @@ public class WordCount
         public <K2 extends Comparable<K2>, V2> List<Record<K2, V2>> map(Record<Integer, String> r)
         {
             List<Record<K2, V2>> res = new ArrayList<>();
-            // todo: 范性使用
+            // todo: 范型如何更好使用
             res.add((Record<K2, V2>) new Record<String, String>(r.getV(), "1"));
             return res;
         }
@@ -40,13 +40,11 @@ public class WordCount
 
     public static void main(String[] args)
     {
-        // todo: recue 处排序有问题
+        // todo: StringInputFormat 切分数据有 bug
         Master<Integer, String, String, String> master = new Master<>();
         master.setMap(new Mapper());
         master.setReduce(new Reducer());
         master.setMaps(2);
-
-        new StringInputFormat("a,b,c,d,e,f,g,a,a,a,b,c,d,d,g",",").partitions(2);
 
         master.setInputFormat(new StringInputFormat("a,b,c,d,e,f,g,a,a,a,b,c,d,d,g",","));
         HashMap<Integer,List<String>> res = master.process();
